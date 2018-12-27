dedaca41f6b50ca6a35478c98496d13310ce3e7c
HeYuyaoo <45375765+HeYuyaoo@users.noreply.github.com>
2018-12-27 21:07:29 +0800

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,17 +1,43 @@
 public class Recursion{
    //part1: factorial non-recursion
-   
+   public static int fac1(int n) {
+        	int tmp=n-1;
+        	while(tmp!=1) {
+        		n=n*(tmp);
+        		tmp--;
+        	}
+        	return n;
+        }
    
    
    //part2: factorial recursion
-   
+   public int fac2(int n) {
+        if(n==1||n==0) {
+             return 1;
+          }
+        return n*factorial(n-1);
+    }
    
    
    //part3: fibonacci sequence non-recursion
-   
+   public int Fibo1(int n){//1 1 3 5 
+          int a=1;
+          if(n>2){
+            while(a<=n-3){
+               int sum= 2+a;
+               a++;
+            }
+             return sum;
+          }
+      return 1;
+         
    
    
    //part4: fibonacci sequence recursion
+    public int Fibo2(int n){
+          if(n==1||n==2) {
+               return 1;
+           }
+           return Fibo(n-1)+Fibo(n-2);
 
-
-}
+    }
